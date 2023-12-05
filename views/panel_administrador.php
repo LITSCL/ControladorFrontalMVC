5b9f4f8e0e2af5796352938fccc3cc5c5ef31d7c
Daniel Álvarez <danielalvarez.litscl.melchiot@protonmail.com>
2023-12-05 14:10:01 -0300

    Ok

---
+++ views/panel_administrador.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8" />
+	<title>Document</title>
+	<?php require_once 'views/includes/head_styles.php'; ?>
+	<?php require_once 'views/includes/head_scripts.php'; ?>
+</head>
+<body>
+	<?php require_once 'views/includes/header.php'; ?>
+	<h1>Panel administrador</h1>
+</body>
+</html>
